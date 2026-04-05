9baa3bd4296c4aa6e6bb7a7bd216272a8ee9cbf6
Meena-Pandiyan <itsmesindhu1999@gmail.com>
2026-04-05 22:42:04 +0530

    add production env and cors fix

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  origin: ["http://localhost:3000", process.env.FRONTEND_URL].filter(Boolean),
   credentials: true
 }));
 app.use(express.json());
